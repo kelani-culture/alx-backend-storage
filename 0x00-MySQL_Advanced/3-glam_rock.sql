e78bcccdbb0186c4a390d308ef71cd17c250c5a1
knull <kelanidarasimi9@gmail.com>
2024-06-13 15:48:55 +0000

    task3 redid

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 --  script that lists all bands with Glam rock as their main
 SELECT
     band_name,
-    (formed - COALESCE(split, '2022-01-01')) AS lifespan
+    (COALESCE(split, 2022) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
