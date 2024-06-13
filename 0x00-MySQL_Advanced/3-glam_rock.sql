e705b4d5aa2ecda5040dd66a0a2f64ef07e1804c
knull <kelanidarasimi9@gmail.com>
2024-06-13 15:50:50 +0000

    task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,3 @@
 --  script that lists all bands with Glam rock as their main
-SELECT
-    band_name,
-    (COALESCE(split, 2022) - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
