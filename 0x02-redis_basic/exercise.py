9eae1c58df13b97559b752440041072a0aed0cbf
Null <kelanidarasimi9@gmail.com>
2024-06-19 22:26:52 +0100

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,9 +12,26 @@ def count_calls(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
+        """functon wrapper"""
         key = f"{method.__qualname__}"
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """call history"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """func wrapper"""
+        key = f"{method.__qualname__}:inputs"
+        self._redis.rpush(key, str(args))
+        result = method(self, *args, *kwargs)
+        key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(key, str(result))
+        return result
 
     return wrapper
 
@@ -24,13 +41,16 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    #@count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """the function store"""
         random_id = str(uuid.uuid4())
         self._redis.set(random_id, data)
         return random_id
 
     def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """the function get"""
         val = self._redis.get(key)
         if not val:
             return None
@@ -39,7 +59,9 @@ class Cache:
         return val
 
     def get_str(self, val: str):
+        """the functon get string"""
         return self.get(val, lambda x: x.decode("utf-8"))
 
     def get_int(self, val: str):
+        """the function get in"""
         return self.get(val, lambda x: int(x))
